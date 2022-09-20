2abc9629b6db37d44e076014ac489a106c29f9c7
Ashere <vip.dogwer@mail.ru>
2022-09-21 03:20:29 +0900

    4.2 Верстка. Первый Scaffold (#6)
    * Windows generate plugin changed.
    
    * font asset fix
    
    * 4.2 Верстка. Первый scaffold.
    
    Co-authored-by: Nikita <ashere.job@mail.ru>

---
+++ lib/screen/sight_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SightLightScreen extends StatefulWidget {
+  const SightLightScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SightLightScreen> createState() => _SightLightScreenState();
+}
+
+class _SightLightScreenState extends State<SightLightScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
