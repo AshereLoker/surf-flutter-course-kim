010eea059ca949ce3f60264db0046d17790c9e30
Ashere <vip.dogwer@mail.ru>
2022-09-22 01:59:47 +0900

    4.3 Верста. RichText и TextSpan (#8)
    * Windows generate plugin changed.
    
    * font asset fix
    
    * 4.3 Верстка. RichText и TextSpan
    
    Co-authored-by: Nikita <ashere.job@mail.ru>

--- lib/screen/sight_list_screen.dart
+++ lib/screen/sight_list_screen.dart
@@ -8,8 +8,44 @@ class SightLightScreen extends StatefulWidget {
 }
 
 class _SightLightScreenState extends State<SightLightScreen> {
+  static const mainColor = Color(0xFF252849);
+  static const secondaryColor = Color(0xFF3B3E5B);
+  static const greenColor = Color(0xFF4CAF50);
+  static const yellowColor = Color(0xFFFCDD3D);
+  static const titleStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    fontSize: 32,
+    height: 36 / 32,
+  );
+  final mainStyle = titleStyle.copyWith(color: mainColor);
+  final secondaryStyle = titleStyle.copyWith(color: secondaryColor);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 40 + 36 * 2,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: RichText(
+          textAlign: TextAlign.left,
+          text: TextSpan(
+            text: 'С',
+            style: titleStyle.copyWith(color: greenColor),
+            children: [
+              TextSpan(text: 'писок', style: secondaryStyle),
+              TextSpan(
+                text: '\nи',
+                style: titleStyle.copyWith(color: yellowColor),
+              ),
+              TextSpan(text: 'нтересных', style: secondaryStyle),
+              TextSpan(text: ' м', style: mainStyle),
+              TextSpan(text: 'ест', style: secondaryStyle),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
