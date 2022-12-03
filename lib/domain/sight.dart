e13d98c57efafc7ec6cd6b67c61f84c1143704cb
Nikita <ashere.job@mail.ru>
2022-12-04 07:09:39 +0900

    4.6 Flutter UI. Mocks.

---
+++ lib/domain/sight.dart
@@ -0,0 +1,15 @@
+class Sight {
+  final String details;
+  final double lat;
+  final double lon;
+  final String name;
+  final String url;
+
+  const Sight({
+    required this.details,
+    required this.lat,
+    required this.lon,
+    required this.name,
+    required this.url,
+  });
+}
